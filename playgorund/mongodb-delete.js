422d07082fe74fa1ab5cff667d6e8090e25adb20
manjunath <manju.paramesan@medialinksaustralia.com.au>
2018-05-10 07:12:07 +1000

    played with delete operations

---
+++ playgorund/mongodb-delete.js
@@ -0,0 +1,61 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+const mongodbUrl = 'mongodb://localhost:27017/TodoApp';
+
+MongoClient.connect(mongodbUrl, (err, client) => {
+    if (err) {
+        console.error('Unable to connect to MongoDB', err);
+    }
+    console.log('Connected to MongoDB server:', mongodbUrl);
+    const db = client.db('TodoApp');
+
+    //delete many
+    // db.collection('Todos')
+    //     .deleteMany({ text: 'Do the laundry' })
+    //     .then((res) => {
+    //         console.log(res);
+    //     })
+    //     .catch((err) => {
+    //         console.error('Error while deleteing items', err);
+    //     });
+
+    //delete one
+    // db.collection('Todos')
+    //     .deleteOne({ text: 'Walk the dog' })
+    //     .then((res) => {
+    //         console.log(res);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
+
+    //find and delete
+    // db.collection('Todos')
+    //     .findOneAndDelete({ completed: false })
+    //     .then((res) => {
+    //         console.log(res);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
+
+    db.collection('Users')
+        .deleteMany({ name: 'Batman' })
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.error('Error while deleteing users', err);
+        });
+
+    db.collection('Users')
+        .findOneAndDelete({ _id: new ObjectID("5af20e665b2cd78caf7e67dc") })
+        .then((res) => {
+            console.log('Delete with ObjectID("5af20e665b2cd78caf7e67dc")', res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    //client.close();
+});

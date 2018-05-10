657279309061a9bcfa3609d3710fe84da2ac104a
manjunath <manju.paramesan@medialinksaustralia.com.au>
2018-05-10 21:59:12 +1000

    Updated file

--- playgorund/mongodb-connect.js
+++ playgorund/mongodb-connect.js
@@ -1,4 +1,3 @@
-// const MongoClient = require('mongodb').MongoClient;
 const { MongoClient, ObjectID } = require('mongodb');
 
 const mongodbUrl = 'mongodb://localhost:27017/TodoApp';
@@ -11,6 +10,27 @@ MongoClient.connect(mongodbUrl, (err, client) => {
 
     const db = client.db('TodoApp');
 
+    // db.collection('Todos').insertOne({
+    //     text: 'Do something',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.error('Unable to insert todo', err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops));
+    // })
+
+    // db.collection('Users').insertOne({
+    //     name: 'Spartan',
+    //     age: 20,
+    //     location: 'Earth'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.error('Unable to insert todo', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops));
+    // });
 
     client.close();
 });
